49d287438b806eb71089113f6d9aae67200c95ac
ATRON3273 <adgjl16430@gmail.com>
2021-05-31 17:20:17 +0900

    add codes for construct frames

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('Hello world');
+console.log('Movie site start');
 
 const express = require('express');
 const app = express();
@@ -17,7 +17,39 @@ app.post('/login', function(req, res) {
     });
     console.log(`id : ${id}, pw : ${pw}`);
 });
- 
+
+app.post('/signup', function(req, res) {
+    const id = req.body.id;
+    const pw = req.body.pw;
+    const nickname = req.body.nickname;
+    const email = req.body.email;
+    // check(id, pw);
+    res.json({
+        message:'success'
+    });
+    console.log(`id : ${id}, pw : ${pw}, nickname : ${nickname}, email : ${email}`);
+});
+
+app.post('/mypage', function(req, res) {
+    const pw = req.body.pw;
+    const nickname = req.body.nickname;
+    const email = req.body.email;
+    // check(id, pw);
+    res.json({
+        message:'success'
+    });
+    console.log(`pw : ${pw}, nickname : ${nickname}, email : ${email}`);
+});
+
+app.post('/', function(req, res) {
+    const search_keyword = req.body.search_keyword;
+    // query(search)
+    res.json({
+        message:'success'
+    });
+    console.log(`search : ${search_keyword}`);
+});
+
 app.get('/abc', function (req, res) {
     res.send('fddsa')
 });
